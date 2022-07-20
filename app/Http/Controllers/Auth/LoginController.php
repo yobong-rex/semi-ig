c15fe72d44a9675b01d4b7050f8e153ac415ca03
Yoga_12 <madeyogabrahmantara@gmail.com>
2022-07-20 10:18:26 +0700

    coba pull dong

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,17 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
+    public function redirectTo()
+        {
+            $password = Auth::user()->password;
+            Auth::logoutOtherDevices($password);
+            if (Auth::user()->role == "Admin") {
+                return redirect()->route('market');
+            } else {
+                return redirect()->route('dashboard');
+            }
+        }
 
     /**
      * Create a new controller instance.
