d2382aff5f1fac35c0d2d891a67e3c483192fe87
alexanderkenrick <alexanderkenrick3@gmail.com>
2022-08-17 22:29:27 +0700

    Update button OP

--- resources/views/Dashboard/dashboard.blade.php
+++ resources/views/Dashboard/dashboard.blade.php
@@ -81,7 +81,7 @@
             .button_OverProduct,
             .gambar_OP {
                 width: 2em;
-                bottom: 10%;
+                bottom: 5%;
                 right: 5%;
             }
 
@@ -106,6 +106,9 @@
             .text_nilai_kartu{
                 font-weight: bold;
                 font-size: 16px;
+            }
+            .text_button_OP{
+                font-size: 10px;
             }
         }
 
@@ -452,7 +455,7 @@
         </div>
 
         {{-- <div style="background:#000000 ;border:1px solid #000000;"> --}}
-        <button class="button_OverProduct" id='btn-op' data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        <button class="button_OverProduct" id='btn-op' data-bs-toggle="modal" data-bs-target="#staticBackdrop" style="width: fit-content;">
             <svg xmlns="http://www.w3.org/2000/svg" class="gambar_OP" fill="currentColor"
                 class="bi bi-exclamation-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
@@ -460,7 +463,7 @@
                     d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
             </svg>
             <br>
-            <span id="OP_text" style="text-align:center;">Over Production</span>
+            <span class="text_button_OP" id="OP_text" style="text-align:center;">Over Production</span>
         </button>
         {{-- </div> --}}
 
